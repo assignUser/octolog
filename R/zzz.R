3bebb7e3f61520bf0b1d3db96c590649cffd8131
assignUser <jacob@wujciak.de>
2022-01-23 19:08:28 +0100

    add cleanup

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,12 @@
-.onLoad <- function(libname, pkgname) {
+.onLoad <- function(libname, pkgname) { # nolint
     if (is.null(getOption("cli.num_colors"))) {
-        enable_github_color()
+        enable_github_colors()
+    }
+}
+
+.onUnload <- function(libname, pkgname) { # nolint
+    if (!is.null(Sys.getenv("R_COLORS_UNSET", NULL))) {
+        Sys.unsetenv("R_COLORS_UNSET")
+        Sys.unsetenv("R_CLI_NUM_COLORS")
     }
 }
