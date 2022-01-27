1fde98cb538512ff93d09a1b800b809ea26754d2
assignUser <jacob@wujciak.de>
2022-01-27 16:34:11 +0100

    try trace

--- .github/workflows/R/conditions.R
+++ .github/workflows/R/conditions.R
@@ -3,6 +3,7 @@
           octo_debug("Tracking the error")
           octo_inform(c("A notice", i = "And some information"))
           octo_warn("A warning")
+          print(rlang::trace_back())
           octo_warn(c("A warning", v = "Without a trace"), trace = NULL)
           octo_abort(c("An error that does not kill R", i = "And how to fix it."), .fail_fast = FALSE)
 
