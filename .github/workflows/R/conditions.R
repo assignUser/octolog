cd50e5f5dcead5f4a40b98e2808ed23cc7655b3b
assignUser <jacob@wujciak.de>
2022-01-29 16:28:32 +0100

    investigate path

--- .github/workflows/R/conditions.R
+++ .github/workflows/R/conditions.R
@@ -1,11 +1,12 @@
           octo_start_group("Conditions")
 
-          octo_echo_on()
+          a <- function(b = rlang::trace_back()) b
+          a()
+          getSrcFilename(a, full.names = TRUE)
           octo_debug("Tracking the error")
           octo_inform(c("A notice", i = "And some information"))
           octo_warn("A warning")
           octo_warn(c("A warning", v = "Without a trace"), trace = NULL)
           octo_abort(c("An error that does not kill R", i = "And how to fix it."), .fail_fast = FALSE)
           octo_echo_off()
-          
-          octo_end_group()
+
