4c346d92fa0942d0bd8e30f6d63b46abab76cb94
assignUser <jacob@wujciak.de>
2022-01-29 17:52:39 +0100

    use rprojroott

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,12 +5,12 @@ library(octolog)
 
 enable_github_colors()
 
-# if (
-#     is.na(
-#         Sys.getenv("OCTOLOG_START_DIR", unset = NA_character_)
-#     )) {
-#     rprojroot::find_package_root_file() %>%
-#         fs::path_tidy() %>%
-#        withr::local_envvar("OCTOLOG_START_DIR" = .)
-# }
+if (
+    is.na(
+        Sys.getenv("OCTOLOG_START_DIR", unset = NA_character_)
+    )) {
+    rprojroot::find_package_root_file() %>%
+        fs::path_tidy() %>%
+       withr::local_envvar("OCTOLOG_START_DIR" = .)
+}
 test_check("octolog")
